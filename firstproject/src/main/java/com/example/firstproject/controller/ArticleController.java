3d562acd91cc449f75da900856f5fe12d4f83cf6
kim-jieun <j2eun0922@naver.com>
2025-02-26 19:23:33 +0900

    자동 커밋: 2025-02-26 19:27:38

--- firstproject/src/main/java/com/example/firstproject/controller/ArticleController.java
+++ firstproject/src/main/java/com/example/firstproject/controller/ArticleController.java
@@ -3,11 +3,15 @@ package com.example.firstproject.controller;
 import com.example.firstproject.dto.ArticleForm;
 import com.example.firstproject.entity.Article;
 import com.example.firstproject.repository.ArticleRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
+@Slf4j
 @Controller
 public class ArticleController {
     @Autowired      //스프링 부트가 미리 생성해 놓은 리파지터리 객체 주입(DI)
@@ -20,13 +24,44 @@ public class ArticleController {
 
     @PostMapping("/articles/create")
     public String createArticle(ArticleForm form){
-        System.out.println(form.toString());
+        //System.out.println(form.toString());
+        log.info (form.toString());
         //1. DTO를 엔티티로 변환
         Article article = form.toEntity();
-        System.out.println(article.toString());
+        //System.out.println(article.toString());
+        log.info (article.toString());
+
         //2. 리파지터리로 엔티티를 db에 저장
         Article saved = articleRepository.save(article);
-        System.out.println(saved.toString());
+        //System.out.println(saved.toString());
+        log.info (saved.toString());
         return "";
+    }
+
+    //데이터 조회 후 출력 (단일 데이터)
+    @GetMapping("/articles/{id}")
+    public String show(@PathVariable Long id, Model model){
+        log.info ("id = " + id);
+        //1.id를 조회해 데이터 가져오기
+        Article articleEntity = articleRepository.findById(id).orElse(null);
+
+        //2.모델에 데이터 등록하기
+        model.addAttribute("article",articleEntity);
+
+        //3.뷰페이지 반환하기
+        return "articles/show";
+    }
+
+    //데이터 목록 조회
+    @GetMapping("/articles")
+    public String index(){
+        //1. DB에서 모든 데이터 가져오기
+        List<Article> articleEntityList = articleRepository.findAll();
+
+        //2. 모델에 데이터 등록하기
+
+
+        //3. 뷰 페이지 설정하기
+        return "/article/index";
     }
 }
--- firstproject/src/main/java/com/example/firstproject/entity/Article.java
+++ firstproject/src/main/java/com/example/firstproject/entity/Article.java
@@ -4,8 +4,14 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
 public class Article {
     @Id
     @GeneratedValue
@@ -14,19 +20,4 @@ public class Article {
     private String title;
     @Column
     private String content;
-
-    public Article(Long id,String title, String content) {
-        this.title = title;
-        this.id = id;
-        this.content = content;
-    }
-
-    @Override
-    public String toString() {
-        return "Article{" +
-                "id=" + id +
-                ", title='" + title + '\'' +
-                ", cntent='" + content + '\'' +
-                '}';
-    }
 }
